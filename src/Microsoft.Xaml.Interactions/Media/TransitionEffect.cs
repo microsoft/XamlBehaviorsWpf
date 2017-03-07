04bcce6cb30b7c3aa3a63c4a1d8efa6359b918cf
Bran Hagger <brhagger@microsoft.com>
2017-03-07 11:55:55 -0800

    Update copyright information to mention MIT license.

--- src/Microsoft.Xaml.Interactions/Media/TransitionEffect.cs
+++ src/Microsoft.Xaml.Interactions/Media/TransitionEffect.cs
@@ -1,6 +1,5 @@
-﻿// -------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All Rights Reserved.
-// -------------------------------------------------------------------
+﻿// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
 namespace Microsoft.Xaml.Interactions.Media
 {
 	using System.Windows;
