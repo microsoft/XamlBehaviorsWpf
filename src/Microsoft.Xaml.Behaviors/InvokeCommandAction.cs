7056143fc87ed3018a92e541cadebfd17aa79eb0
Brian Lagunas <brianlagunas@users.noreply.github.com>
2018-12-10 21:52:00 -0700

    added EventArgsParameterPath property

--- src/Microsoft.Xaml.Behaviors/InvokeCommandAction.cs
+++ src/Microsoft.Xaml.Behaviors/InvokeCommandAction.cs
@@ -21,6 +21,7 @@ namespace Microsoft.Xaml.Behaviors
         public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.Register("CommandParameter", typeof(object), typeof(InvokeCommandAction), null);
         public static readonly DependencyProperty EventArgsConverterProperty = DependencyProperty.Register("EventArgsConverter", typeof(IValueConverter), typeof(InvokeCommandAction), new PropertyMetadata(null));
         public static readonly DependencyProperty EventArgsConverterParameterProperty = DependencyProperty.Register("EventArgsConverterParameter", typeof(object), typeof(InvokeCommandAction), new PropertyMetadata(null));
+        public static readonly DependencyProperty EventArgsParameterPathProperty = DependencyProperty.Register("EventArgsParameterPath", typeof(string), typeof(InvokeCommandAction), new PropertyMetadata(null));
 
         /// <summary>
         /// Gets or sets the name of the command this action should invoke.
@@ -88,6 +89,15 @@ namespace Microsoft.Xaml.Behaviors
         }
 
         /// <summary>
+        /// Gets or sets the parameter path used to extract a value from an <see cref= "EventArgs" /> property to pass to the Command as a parameter.
+        /// </summary>
+        public string EventArgsParameterPath
+        {
+            get { return (string)GetValue(EventArgsParameterPathProperty); }
+            set { SetValue(EventArgsParameterPathProperty, value); }
+        }
+
+        /// <summary>
         /// Specifies whether the EventArgs of the event that triggered this action should be passed to the Command as a parameter.
         /// </summary>
         public bool PassEventArgsToCommand { get; set; }
@@ -104,8 +114,13 @@ namespace Microsoft.Xaml.Behaviors
 
                 if (command != null)
                 {
-                    var commandParameter = this.CommandParameter;
-                    if (commandParameter == null && PassEventArgsToCommand)
+                    object commandParameter = this.CommandParameter;
+                    if (commandParameter == null && !string.IsNullOrWhiteSpace(this.EventArgsParameterPath))
+                    {
+                        commandParameter = GetEventArgsPropertyPathValue(parameter);
+                    }
+
+                    if (commandParameter == null && this.PassEventArgsToCommand)
                     {
                         commandParameter = EventArgsConverter == null ? parameter : EventArgsConverter.Convert(parameter, typeof(object), EventArgsConverterParameter, CultureInfo.CurrentCulture);
                     }
@@ -119,6 +134,21 @@ namespace Microsoft.Xaml.Behaviors
                     Debug.WriteLine(ExceptionStringTable.CommandDoesNotExistOnBehaviorWarningMessage, this.CommandName, this.AssociatedObject.GetType().Name);
                 }
             }
+        }
+
+        private object GetEventArgsPropertyPathValue(object parameter)
+        {
+            object commandParameter;
+            object propertyValue = parameter;
+            string[] propertyPathParts = EventArgsParameterPath.Split('.');
+            foreach (string propertyPathPart in propertyPathParts)
+            {
+                PropertyInfo propInfo = propertyValue.GetType().GetProperty(propertyPathPart);
+                propertyValue = propInfo.GetValue(propertyValue, null);
+            }
+
+            commandParameter = propertyValue;
+            return commandParameter;
         }
 
         private ICommand ResolveCommand()
