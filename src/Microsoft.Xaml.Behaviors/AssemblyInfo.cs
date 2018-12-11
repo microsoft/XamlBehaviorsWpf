69300a33184ff44a3469acbc4a3901490a9976bb
Morten Nielsen <no@spam.thx>
2018-12-10 23:10:26 -0800

    Move project to use csproj. - Remove strongnaming when building without SignReal set. - Remove strongnaming from unit test all-together (serves no purpose and only complicates building) - Remove scripts folder - csproj now takes care of all nuget packaging.

--- src/Microsoft.Xaml.Behaviors/AssemblyInfo.cs
+++ src/Microsoft.Xaml.Behaviors/AssemblyInfo.cs
@@ -8,21 +8,6 @@ using System.Runtime.CompilerServices;
 using System.Windows.Markup;
 using Microsoft.Xaml.Behaviors;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Xaml.Behaviors")]
-[assembly: AssemblyDescription("Microsoft.Xaml.Behaviors")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("Microsoft.Xaml.Behaviors")]
-[assembly: AssemblyCopyright("Copyright (c) Microsoft Corporation. All rights reserved.")]
-
-[assembly: AssemblyVersion(RuntimeVersion.AssemblyVersion)]
-[assembly: AssemblyFileVersion(VersionConstants.AssemblyFileVersion)]
-
-[assembly: AssemblyKeyFile("FinalPublicKey.snk")]
-[assembly: AssemblyDelaySign(true)]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -54,4 +39,10 @@ using Microsoft.Xaml.Behaviors;
 
 [assembly: SuppressMessage("Microsoft.Design", "CA1020:AvoidNamespacesWithFewTypes", Scope = "namespace", Target = "Microsoft.Xaml.Behaviors")]
 
+#if SIGN_REAL
+[assembly: AssemblyKeyFile("FinalPublicKey.snk")]
+[assembly: AssemblyDelaySign(true)]
 [assembly: InternalsVisibleTo("UnitTests, PublicKey='002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
+#else
+[assembly: InternalsVisibleTo("UnitTests")]
+#endif
