90a868e54dd36d12b71b48cbd590a2bde3759a02
Daniel Vaughan <davaugh@microsoft.com>
2019-11-21 14:04:02 -0800

    Changes in response to comments 1

--- src/Microsoft.Xaml.Behaviors.Design/Properties/AssemblyInfo.cs
+++ src/Microsoft.Xaml.Behaviors.Design/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6fba03ac-d43f-447f-a087-404b9db14952")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
